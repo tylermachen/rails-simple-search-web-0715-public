f5c3a78110dc7dc29aaa35287ccf2f032843989f
Tyler Machen <tylermachen@gmail.com>
2015-09-02 14:13:42 -0400

    done searching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,3 +1,10 @@
 class SearchController < ApplicationController
-
+  def index
+    if word = Word.find_by(name: params[:keyword])
+      redirect_to word
+    else
+      @results = Search.for(params[:keyword])
+      @results = "No results matching that query." if @results.empty?
+    end
+  end
 end
--- db/migrate/20140911162311_create_words.rb
+++ db/migrate/20140911162311_create_words.rb
@@ -1,2 +1,8 @@
 class CreateWords < ActiveRecord::Migration
+  def change
+    create_table :words do |t|
+      t.string   :name
+      t.timestamps null: false
+    end
+  end
 end
